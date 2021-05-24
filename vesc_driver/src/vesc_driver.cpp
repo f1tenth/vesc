d9a8c9be43080c648e2e3f2b89f2340f4b26d1fd
Andrea Scipione <andrea.scipione@gmail.com>
2021-05-24 00:37:10 -0700

    fix test results

--- vesc_driver/src/vesc_driver.cpp
+++ vesc_driver/src/vesc_driver.cpp
@@ -65,9 +65,11 @@ VescDriver::VescDriver(const rclcpp::NodeOptions & options)
   fw_version_minor_(-1)
 {
   // get vesc serial port address
-  // default to /tmp/ttyV0 for debug purpose
-  // use socat /dev/ttyACM0,raw,echo=0  SYSTEM:'tee in.txt |socat - "PTY,link=/tmp/ttyV0,raw,echo=0,waitslave" |tee out.txt'
-  // to sniff comunication data to and from vesc
+  /* default to /tmp/ttyV0 for debug purpose
+   * use :
+   *  socat /dev/ttyACM0,raw,echo=0  SYSTEM:'tee in.txt |socat - "PTY,link=/tmp/ttyV0,raw,echo=0,waitslave" |tee out.txt'
+   * to sniff comunication data to and from vesc
+   */
   std::string port = declare_parameter<std::string>("port", "/tmp/ttyV0");
 
   // attempt to connect to the serial port
