b3b6501b4c1d37b2577e54966d05626ebde6cfb2
Andrea Scipione <andrea.scipione@gmail.com>
2021-06-22 18:26:48 +0200

    fixed timing wait for imu polling

--- vesc_driver/src/vesc_interface.cpp
+++ vesc_driver/src/vesc_interface.cpp
@@ -162,7 +162,7 @@ void VescInterface::Impl::rxThread()
       }
     }
     // Only attempt to read every 10 ms
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+//    std::this_thread::sleep_for(std::chrono::milliseconds(5));
   }
 }
 
