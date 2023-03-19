ce260f89826dfa5e762db8acc750a910c8aef822
Valerio Magnago <magnago@magazino.eu>
2023-03-19 02:05:00 +0100

    Fix compilation on ros humble.

--- vesc_ackermann/src/ackermann_to_vesc.cpp
+++ vesc_ackermann/src/ackermann_to_vesc.cpp
@@ -48,10 +48,12 @@ AckermannToVesc::AckermannToVesc(const rclcpp::NodeOptions & options)
 : Node("ackermann_to_vesc_node", options)
 {
   // get conversion parameters
-  speed_to_erpm_gain_ = declare_parameter("speed_to_erpm_gain").get<double>();
-  speed_to_erpm_offset_ = declare_parameter("speed_to_erpm_offset").get<double>();
-  steering_to_servo_gain_ = declare_parameter("steering_angle_to_servo_gain").get<double>();
-  steering_to_servo_offset_ = declare_parameter("steering_angle_to_servo_offset").get<double>();
+  speed_to_erpm_gain_ = declare_parameter<double>("speed_to_erpm_gain");
+  speed_to_erpm_offset_ = declare_parameter<double>("speed_to_erpm_offset");
+  steering_to_servo_gain_ =
+    declare_parameter<double>("steering_angle_to_servo_gain");
+  steering_to_servo_offset_ =
+    declare_parameter<double>("steering_angle_to_servo_offset");
 
   // create publishers to vesc electric-RPM (speed) and servo commands
   erpm_pub_ = create_publisher<Float64>("commands/motor/speed", 10);
