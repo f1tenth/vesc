bf6e4d098b76ac2b9f4e95a55a1baa50eef64109
VineetTambe <tambevineet@gmail.com>
2023-04-11 17:04:55 -0400

    restored liscences to all files

--- vesc_driver/include/vesc_driver/datatypes.h
+++ vesc_driver/include/vesc_driver/datatypes.h
@@ -1,5 +1,30 @@
+// Copyright 2020 F1TENTH Foundation
+//
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+//    and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list
+//    of conditions and the following disclaimer in the documentation and/or other materials
+//    provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors may be used
+//    to endorse or promote products derived from this software without specific prior
+//    written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+// WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 /*
-    Copyright 2016 - 2017 Benjamin Vedder	benjamin@vedder.se
+    Copyright 2016 - 2017 Benjamin Vedder benjamin@vedder.se
 
     This file is part of VESC Tool.
 
@@ -15,7 +40,7 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    */
+*/
 
 #ifndef DATATYPES_H
 #define DATATYPES_H
@@ -206,48 +231,6 @@ typedef enum
   COMM_SET_CAN_MODE,
   COMM_GET_IMU_CALIBRATION
 } COMM_PACKET_ID;
-
-// typedef enum {
-//     COMM_FW_VERSION = 0,
-//     COMM_JUMP_TO_BOOTLOADER,
-//     COMM_ERASE_NEW_APP,
-//     COMM_WRITE_NEW_APP_DATA,
-//     COMM_GET_VALUES,
-//     COMM_SET_DUTY,
-//     COMM_SET_CURRENT,
-//     COMM_SET_CURRENT_BRAKE,
-//     COMM_SET_RPM,
-//     COMM_SET_POS,
-//     COMM_SET_HANDBRAKE,
-//     COMM_SET_DETECT,
-//     COMM_SET_SERVO_POS,
-//     COMM_SET_MCCONF,
-//     COMM_GET_MCCONF,
-//     COMM_GET_MCCONF_DEFAULT,
-//     COMM_SET_APPCONF,
-//     COMM_GET_APPCONF,
-//     COMM_GET_APPCONF_DEFAULT,
-//     COMM_SAMPLE_PRINT,
-//     COMM_TERMINAL_CMD,
-//     COMM_PRINT,
-//     COMM_ROTOR_POSITION,
-//     COMM_EXPERIMENT_SAMPLE,
-//     COMM_DETECT_MOTOR_PARAM,
-//     COMM_DETECT_MOTOR_R_L,
-//     COMM_DETECT_MOTOR_FLUX_LINKAGE,
-//     COMM_DETECT_ENCODER,
-//     COMM_DETECT_HALL_FOC,
-//     COMM_REBOOT,
-//     COMM_ALIVE,
-//     COMM_GET_DECODED_PPM,
-//     COMM_GET_DECODED_ADC,
-//     COMM_GET_DECODED_CHUK,
-//     COMM_FORWARD_CAN,
-//     COMM_SET_CHUCK_DATA,
-//     COMM_CUSTOM_APP_DATA,
-//     COMM_NRF_START_PAIRING,
-//     COMM_GET_IMU_DATA
-// } COMM_PACKET_ID;
 
 typedef struct
 {
